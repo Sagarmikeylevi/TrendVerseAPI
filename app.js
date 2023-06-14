35376380fdf9443f419da38bf8dde808f55dece4
unknown <sagarmikeylevi@gmail.com>
2023-06-14 21:32:51 +0530

    connected to mongodb Atlas

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT;
+const mongoose = require("./config/mongoose");
+
+app.use(express.json()); // To parse JSON request bodies
+app.use(express.urlencoded({ extended: false })); // To parse URL-encoded request bodies
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
+
+  console.log(`Server is running on port: ${port}`);
+});
+
+/*
+
+// 1. authentication 
+
+*/
